8db0e74b0ca946e9220576aad1985f79c6512757
L0mchansky <persmax13@gmail.com>
2026-03-26 02:41:23 +0300

    refactor

--- Assets/Scripts/m29_30_task_1/Currency/IReadOnlyCurrency.cs
+++ Assets/Scripts/m29_30_task_1/Currency/IReadOnlyCurrency.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace m29_30_task_1
+﻿namespace m29_30_task_1
 {
     public interface IReadOnlyCurrency
     {
